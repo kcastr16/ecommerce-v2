606aa68c9c8faa6d3b1d3024989d8e704f6d3455
Kevin Guzman <kevincastrog28@gmail.com>
2025-01-29 00:29:41 -0500

    hero img for contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,13 @@ const Contact = () => {
     return (
         <>
          <h1>Contact Page</h1>
+         <div className="hero-img-cont">
+            <div className="hero-img-main">
+                <h1>Contact Us!</h1>
+                <p className="hero-img-txt">We'd love to hear any ideas or concerns you'd have. We'll see your message at the finish line!</p>
+            </div>
+            <img src="https://global.discourse-cdn.com/forza/original/4X/5/5/2/5520d16472f8eaef1fbf83c240d246e67b861646.png" alt="Porsche Badge"></img>
+        </div>
         </>
     )
 }
